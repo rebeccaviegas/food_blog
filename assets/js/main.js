8aad6b000bac637445f93faecff61565f05ca45d
Rebecca Viegas <you@example.com>
2018-06-19 21:05:46 -0400

    removed nav, removed section id classes

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,9 @@ win.onscroll = function(){
    var sTop = (this.pageYOffset || docEl.scrollTop)  - (docEl.clientTop || 0);
    $logo.style.backgroundColor =  sTop < 300 ? "rgba(255,255,255,1)":"rgba(255,255,255,0.5)" ;
 };
+
+$('.scroll').click(function() {
+    $('body').animate({
+        scrollTop: eval($('#' + $(this).attr('target')).offset().top - 70)
+    }, 1000);
+});
